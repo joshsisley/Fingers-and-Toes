5b56fe290886a02a462d25314ff5b5636f1f29ba
joshuasisley <joshua.sisley@gmail.com>
2016-01-31 16:06:54 -0600

    Got the functionality for the highlighting of the specifics blocks based on the count up number. Still have to work on the last two buttons and design

--- mainScript.js
+++ mainScript.js
@@ -11,15 +11,56 @@ function startTimer() {
 
 function countUp(maxNumber) {
     var baseNum = 1;
+    var displayedNumber = document.getElementById('displayed-number');
+    var fingersDiv = document.getElementById('fingers-box');
+    var toesDiv = document.getElementById('toes-box');
     var intervalId = setInterval(function(){
         console.log('interval gets called');
         var newNum = baseNum++;
         if (newNum < maxNumber) {
-            console.log(newNum);
-            document.getElementById('displayed-number').innerHTML = newNum.toString();
+            if (newNum % 5 === 0 && newNum % 3 === 0) {
+                fingersDiv.style.backgroundColor = 'green';
+                toesDiv.style.backgroundColor = 'red';
+                displayedNumber.innerHTML = newNum.toString();
+            }
+            else if (newNum % 5 === 0) {
+                fingersDiv.style.backgroundColor = 'transparent';
+                toesDiv.style.backgroundColor = 'red';
+                displayedNumber.innerHTML = newNum.toString();
+            }
+            else if (newNum % 3 === 0) {
+                toesDiv.style.backgroundColor = 'transparent';
+                fingersDiv.style.backgroundColor = 'green';
+                displayedNumber.innerHTML = newNum.toString();
+            }
+            else {
+                toesDiv.style.backgroundColor = 'transparent';
+                fingersDiv.style.backgroundColor = 'transparent';
+                displayedNumber.innerHTML = newNum.toString();
+            }
         }
         if (newNum == maxNumber) {
-            document.getElementById('displayed-number').innerHTML = newNum.toString();
+            if (newNum % 5 === 0 && newNum % 3 === 0) {
+                fingersDiv.style.backgroundColor = 'green';
+                toesDiv.style.backgroundColor = 'red';
+                displayedNumber.innerHTML = newNum.toString();
+            }
+            else if (newNum % 5 === 0) {
+                fingersDiv.style.backgroundColor = 'transparent';
+                toesDiv.style.backgroundColor = 'red';
+                displayedNumber.innerHTML = newNum.toString();
+            }
+            else if (newNum % 3 === 0) {
+                toesDiv.style.backgroundColor = 'transparent';
+                fingersDiv.style.backgroundColor = 'green';
+                displayedNumber.innerHTML = newNum.toString();
+            }
+            else {
+                toesDiv.style.backgroundColor = 'transparent';
+                fingersDiv.style.backgroundColor = 'transparent';
+                displayedNumber.innerHTML = newNum.toString();
+            }
+            displayedNumber.innerHTML = newNum.toString();
             clearInterval(intervalId);
         }
     }, 1000);
