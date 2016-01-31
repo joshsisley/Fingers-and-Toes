e52e80c9f0b81ca8b47c595c3e77cf2fca10165b
joshuasisley <joshua.sisley@gmail.com>
2016-01-31 15:08:05 -0600

    Got the basic counter functionality working

--- mainScript.js
+++ mainScript.js
@@ -1,3 +1,26 @@
 /**
  * Created by joshuasisley on 1/30/16.
  */
+// Starts the timer when the button is pressed
+function startTimer() {
+    var inputNumber = document.getElementById('number-input').value;
+    console.log('here is the input number');
+    console.log(inputNumber);
+    countUp(inputNumber);
+}
+
+function countUp(maxNumber) {
+    var baseNum = 1;
+    var intervalId = setInterval(function(){
+        console.log('interval gets called');
+        var newNum = baseNum++;
+        if (newNum < maxNumber) {
+            console.log(newNum);
+            document.getElementById('displayed-number').innerHTML = newNum.toString();
+        }
+        if (newNum == maxNumber) {
+            document.getElementById('displayed-number').innerHTML = newNum.toString();
+            clearInterval(intervalId);
+        }
+    }, 1000);
+}
